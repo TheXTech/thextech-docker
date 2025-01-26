3b7bebb973affaa213946f5e4a0baf3b2146b028
Wohlstand <admin@wohlnet.ru>
2025-01-26 18:27:07 +0300

    Added script to rebuild and push the thing immediately

---
+++ build-and-push-all.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+for q in \
+    ubuntu-16-04-32bit-kit \
+    ubuntu-16-04-64bit-kit \
+    ubuntu-18-04 \
+    ubuntu-20-04 \
+    ubuntu-22-04 \
+    ubuntu-24-04 \
+    ubuntu-24-04-dkp-plus-vita \
+; do
+    echo "------------------------------ $q -------------------------------------"
+    ./build-one-full.sh $q
+
+    cd $q
+    ./push.sh
+    echo "Clean-up..."
+    docker-compose down -v --rmi all --remove-orphans
+    cd ..
+done
